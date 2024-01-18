d46bab2e8f49d097c56a68aaaece5e2fcfca91ab
asusikai <75575520+asusikai@users.noreply.github.com>
2024-01-19 00:28:33 +0900

    컨벤션 수정

--- BOJ_16928/Main.java
+++ BOJ_16928/Main.java
@@ -5,46 +5,47 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N,M;
+    static int N, M;
     static int[] board = new int[101];
     static int minCount = 0;
-     static void bfs(){
+
+    static void bfs() {
 
         boolean[] visited = new boolean[101];
         Queue<int[]> positionQ = new LinkedList<>();
         positionQ.add(new int[]{1, 0});
         visited[1] = true;
 
-        while(!positionQ.isEmpty()){
+        while (!positionQ.isEmpty()) {
             int[] current = positionQ.poll();
             int pos = board[current[0]];
             int cnt = current[1];
 
-            if(pos == 100){
+            if (pos == 100) {
                 minCount = cnt;
             }
 
-            for(int i = 1; i<=6; i++){
-                if((pos+i) <= 100 && !visited[pos+i]){
-                    visited[pos+i] = true;
-                    positionQ.add(new int[]{pos+i, cnt+1});
+            for (int i = 1; i <= 6; i++) {
+                if ((pos + i) <= 100 && !visited[pos + i]) {
+                    visited[pos + i] = true;
+                    positionQ.add(new int[]{pos + i, cnt + 1});
                 }
             }
         }
     }
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        for(int i = 1; i<=100; i++){
+        for (int i = 1; i <= 100; i++) {
             board[i] = i;
         }
 
-        for(int i = 0; i<N+M; i++){
+        for (int i = 0; i < N + M; i++) {
             st = new StringTokenizer(br.readLine());
             board[Integer.parseInt(st.nextToken())] = Integer.parseInt(st.nextToken());
         }
