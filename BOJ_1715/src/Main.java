5885f968b5465f10f562f5c62695e5abd02a0804
asusikai <75575520+asusikai@users.noreply.github.com>
2024-03-31 22:32:44 +0900

    카드 정렬하기 해결

---
+++ BOJ_1715/src/Main.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(N);
+
+        for (int i = 0; i < N; i++) {
+            pq.add(Integer.parseInt(br.readLine()));
+        }
+        int answer = 0;
+        while (pq.size() > 1) {
+            int add = pq.poll();
+            add += pq.poll();
+            answer += add;
+            pq.add(add);
+        }
+
+        System.out.println(answer);
+    }
+}
